3d947a1fc31d99ed296445ed6f3bb635defddc16
tenbyadventure <info@tenbyadventure.co.uk>
2025-11-19 19:13:43 +0000

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE = 'tig-preset-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
+      if (e.request.method === 'GET' && resp.status === 200 && resp.headers.get('content-type')?.includes('text/')) {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+      }
+      return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
